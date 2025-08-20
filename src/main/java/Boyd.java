05f774b7a780e39e12b8d8681103d1bdf76a3626
Sean Ow <sean.ow25@gmail.com>
2025-08-20 15:58:00 +0800

    Added task encapsulation, mark feature

--- src/main/java/Boyd.java
+++ src/main/java/Boyd.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Boyd {
-    private static String[] mem = new String[100];
+    private static Task[] mem = new Task[100];
     private static int itemCount = 0;
     private static String line = "____________________________________________________________";
     private static String chatbotName = "Boyd";
@@ -39,6 +39,32 @@ public class Boyd {
             if (command.equalsIgnoreCase("bye")) {
                 break;
             }
+            if (command.startsWith("mark")) {
+                if (command.length() == 6) {
+                    int itemNo = Integer.parseInt(command.substring(5)); //convert to integer
+                    if (itemNo > itemCount) {
+                        System.out.println(line);
+                        System.out.println("Invalid item number");
+                        System.out.println(line);
+                        continue;
+                    }
+                    if (itemCount == 0) {
+                        System.out.println(line);
+                        System.out.println("No items yet!");
+                        System.out.println(line);
+                    }
+                    mem[itemNo - 1].markAsDone();
+                    System.out.println(line);
+                    String message = "Nice! I've marked this task as done:\n" + mem[itemNo - 1].getDescription();
+                    System.out.println(message);
+                    System.out.println(line);
+                } else {
+                    System.out.println(line);
+                    System.out.println("Command should be of the format: \"mark <number>\"");
+                    System.out.println(line);
+                }
+                continue;
+            }
             if (command.equalsIgnoreCase("list")) {
                 System.out.println(line);
                 if (itemCount == 0) {
@@ -47,12 +73,12 @@ public class Boyd {
                     continue;
                 }
                 for (int i = 0; i < itemCount; i++) {
-                    System.out.println(i + ". " + mem[i]);
+                    System.out.println((i + 1) + ". " + mem[i].getDescription());
                 }
                 System.out.println(line);
                 continue;
             }
-            mem[itemCount] = command;
+            mem[itemCount] = new Task(command);
             System.out.println(line);
             System.out.println("added: " + command);
             System.out.println(line);
