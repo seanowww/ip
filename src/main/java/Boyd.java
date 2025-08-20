a10f7484d9a9d6b933486ee3713a9ca9269bc66b
Sean Ow <sean.ow25@gmail.com>
2025-08-20 15:04:28 +0800

    Echo function

--- src/main/java/Boyd.java
+++ src/main/java/Boyd.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Boyd {
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
@@ -7,6 +9,7 @@ public class Boyd {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
         greet();
+        echo();
     }
     public static void greet() {
         String chatbotName = "Boyd";
@@ -14,7 +17,28 @@ public class Boyd {
         System.out.println(line);
         System.out.println("Hello! I'm " + chatbotName + "!\nWhat can I do for you?");
         System.out.println(line);
+    }
+    public static void bye() {
+        String line = "____________________________________________________________";
+        System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
     }
+
+    public static void echo() {
+        String chatbotName = "Boyd";
+        String line = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+        String command = "";
+        while (true) {
+            command = scanner.nextLine();
+            if (command.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println(line);
+            System.out.println(command);
+            System.out.println(line);
+        }
+        bye();
+        }
 }
