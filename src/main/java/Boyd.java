0881ea266a84a7259e28f9c254e7d32d99c2cd02
Sean Ow <sean.ow25@gmail.com>
2025-08-20 15:20:10 +0800

    List function

--- src/main/java/Boyd.java
+++ src/main/java/Boyd.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Boyd {
+    private static String[] mem = new String[100];
+    private static int itemCount = 0;
+    private static String line = "____________________________________________________________";
+    private static String chatbotName = "Boyd";
+
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -11,13 +16,13 @@ public class Boyd {
         greet();
         echo();
     }
+
     public static void greet() {
-        String chatbotName = "Boyd";
-        String line = "____________________________________________________________";
         System.out.println(line);
         System.out.println("Hello! I'm " + chatbotName + "!\nWhat can I do for you?");
         System.out.println(line);
     }
+
     public static void bye() {
         String line = "____________________________________________________________";
         System.out.println(line);
@@ -27,7 +32,6 @@ public class Boyd {
 
     public static void echo() {
         String chatbotName = "Boyd";
-        String line = "____________________________________________________________";
         Scanner scanner = new Scanner(System.in);
         String command = "";
         while (true) {
@@ -35,10 +39,25 @@ public class Boyd {
             if (command.equalsIgnoreCase("bye")) {
                 break;
             }
+            if (command.equalsIgnoreCase("list")) {
+                System.out.println(line);
+                if (itemCount == 0) {
+                    System.out.println("You haven't added any items!");
+                    System.out.println(line);
+                    continue;
+                }
+                for (int i = 0; i < itemCount; i++) {
+                    System.out.println(i + ". " + mem[i]);
+                }
+                System.out.println(line);
+                continue;
+            }
+            mem[itemCount] = command;
             System.out.println(line);
-            System.out.println(command);
+            System.out.println("added: " + command);
             System.out.println(line);
+            itemCount++;
         }
         bye();
-        }
+    }
 }
