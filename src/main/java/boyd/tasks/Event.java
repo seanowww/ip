038d627f894cba4907ce724156f616b588ad4d83
seanowww <96902684+seanowww@users.noreply.github.com>
2025-09-10 14:28:25 +0800

    Update Event.java

--- src/main/java/boyd/tasks/Event.java
+++ src/main/java/boyd/tasks/Event.java
@@ -30,11 +30,12 @@ public class Event extends Task {
     @Override
     public String toDataString() {
         return String.format(
-                "E | %d | %s | %s - %s", (
+                "E | %d | %s | %s - %s | %s", (
                         this.isDone ? 1 : 0),
                 getDescription(),
                 from,
-                to
+                to,
+                super.formatTags()
         );
     }
 }
